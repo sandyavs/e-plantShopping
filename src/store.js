4fe43f2904d2f8b167994ef0ca10b81532c79906
“sandyavs” <sandyasadasivam01@gmail.com>
2025-07-27 23:27:58 -0400

    format doc tab

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+const store = configureStore({
+	// Define the root reducer object
+	reducer: {
+		cart: cartReducer, // 'cart' is the name of the slice in the store, and it's managed by cartReducer
+	},
 });
 export default store
