5efdd20763cd9c8fdf4c0ca93113344e521aab4a
“sandyavs” <sandyasadasivam01@gmail.com>
2025-07-27 21:41:05 -0400

    added cart reducers and action creators

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
+	name: 'cart',
+	initialState: {
+		items: [], // Initialize items as an empty array
+	},
+	reducers: {
+		//plant: name, img, descr, cost
+		addItem: (state, action) => {
+			//Destructure action payload
+			const {name, image, cost} = action.payload;
+			const existingItem = state.items.find(item => item.name === name);
+			//If item already exists incr the count
+			if(existingItem) {
+				existingItem.quantity += 1;
+			} else { //if not, add it to cart with quantity: 1
+				state.items.push({name, image, cost, quantity: 1});
+			}
+		},
+		removeItem: (state, action) => {
+			state.items = state.items.filter(item => item.name !== action.payload);
+		},
+		updateQuantity: (state, action) => {
+			const {name, quantity} = action.payload;
+			const itemToUpdate = state.items.find(item => item.name === name);
+			if(itemToUpdate) {
+				itemToUpdate.quantity += 1;
+			}
+		},
+	},
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
